5f7d689e9662e56452cea7a20374bad1a84de6ef
TomCourant <tomcourant@gmail.com>
2018-02-07 15:36:45 +0100

    map partials & add libe-bulma css

--- build.js
+++ build.js
@@ -3,41 +3,35 @@ const fs = require('fs'),
       dataView = require('./dataView'),
       templateType = process.argv[2] || false
 
-var mainVars = {}
+var mainVars = {
+  base: null,
+  partials: {}
+}
 
 if (templateType) {
 
   dataView.page.trackindSlug = dataView.page.year + '-' + dataView.page.month + '-' + dataView.page.slug
 
-  // load all templates
-  let allTemplates = ['chapo', 'columnsMultiline', 'head', 'headerTop', 'social', 'tabs', 'tracking', 'sideLeft', 'mainRight']
-
-  // template base
+  // load base template
   mainVars[templateType] = fs.readFileSync('./templates/' + templateType + '.mustache').toString()
 
-  // template partials
-  let chapo = fs.readFileSync('./templates/_partials/chapo.mustache').toString(),
-      columnsMultiline = fs.readFileSync('./templates/_partials/columnsMultiline.mustache').toString(),
-      head = fs.readFileSync('./templates/_partials/head.mustache').toString(),
-      headerTop = fs.readFileSync('./templates/_partials/headerTop.mustache').toString(),
-      social = fs.readFileSync('./templates/_partials/social.mustache').toString(),
-      tabs = fs.readFileSync('./templates/_partials/tabs.mustache').toString(),
-      tracking = fs.readFileSync('./templates/_partials/tracking.mustache').toString(),
-      sideLeft = fs.readFileSync('./templates/_partials/sideLeft.mustache').toString(),
-      mainRight = fs.readFileSync('./templates/_partials/mainRight.mustache').toString()
+  // load all partials templates
+  let allTemplates = ['chapo', 'columnsMultiline', 'head', 'headerTop', 'social', 'tabs', 'tracking', 'sideLeft', 'mainRight']
+  allTemplates.forEach(partials => {
+    mainVars.partials[partials] = fs.readFileSync('./templates/_partials/' + partials + '.mustache').toString()
+  })
 
   // mustache render
-  let index = Mustache.render(mainVars[templateType], dataView, {
-    chapo, columnsMultiline, head, headerTop, social, tabs, tracking, sideLeft, mainRight
-  })
+  let index = Mustache.render(mainVars[templateType], dataView, mainVars.partials)
 
   // write html
   fs.writeFile('./dist/index.html', index.toString(), function (err) {
     if (err) console.log(err);
-    console.log('./dist/index.html have benn created succesfuly !');
+    console.log('./dist/index.html have been created succesfuly !');
+    fs.createReadStream('./node_modules/libe-bulma/css/six-plus.css').pipe(fs.createWriteStream('./dist/static/css/six-plus.css'))
+    console.log('./dist/static/css/six-plus.css have benn copyed from libe-bulma succesfuly !');
+    console.log('template ' + templateType + ' is ready in ./dist !')
   });
 
-} else {
-  console.log('please defined a template type !')
 }
 
